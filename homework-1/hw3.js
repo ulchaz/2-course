f6b89cba06c73d353aed7a5c1cddc4357b396055
ulchaz <uliaesaulkova48@gmail.com>
2025-02-04 13:15:06 +0300

    задание 5

--- homework-1/hw3.js
+++ homework-1/hw3.js
@@ -1,4 +1,43 @@
-let a = '2';
-let b = '3';
-// Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
-alert(Number(a) + Number(b));
+let day = prompt('Введите номер месяца')
+
+switch (day) {
+    case '1':
+        alert('январь')
+        break;
+    case '2':
+        alert('февраль')
+        break;
+    case '3':
+        alert('март')
+        break;
+    case '4':
+        alert('апрель')
+        break;
+    case '5':
+        alert('май')
+        break;
+    case '6':
+        alert('июнь')
+        break;
+    case '7':
+        alert('июль')
+        break;
+    case '8':
+        alert('август')
+        break;
+    case '9':
+        alert('сентябрь')
+        break;
+    case '10':
+        alert('октябрь')
+        break;
+    case '11':
+        alert('ноябрь')
+        break;
+    case '12':
+        alert('декабрь')
+        break;
+
+    default: alert('Неккоректное число');
+        break;
+}
