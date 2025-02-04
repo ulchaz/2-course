023138e31dcde80b3a5713e5b904580b6fe7f3b0
ulchaz <uliaesaulkova48@gmail.com>
2025-02-04 12:40:06 +0300

    задание 1

---
+++ homework-1/hw3.js
@@ -0,0 +1,7 @@
+let password = 'qwerty';
+let userPassword = prompt('Введите пароль')
+if (userPassword === password) {
+    alert('Пароль введен верно')
+} else {
+    alert('Пароль введен неверно')
+}
