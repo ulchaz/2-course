476d62d824ea4264401f468ce01fe0c10bb07388
ulchaz <uliaesaulkova48@gmail.com>
2025-02-05 15:16:48 +0300

    задание 4

--- homework-1/hw4.js
+++ homework-1/hw4.js
@@ -1,6 +1,9 @@
-let i = 7;
-
-while (i <=22 ){
-    console.log(i);
-    i++;
+let obj = {
+    "Коля" : '200',
+    "Вася" : '300',
+    "Петя" : '400'
+}
+for (let name in obj){
+    let salary = obj[name];
+    console.log(`${name} — зарплата ${salary} долларов`)
 }
